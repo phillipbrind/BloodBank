a8054f28870ad742a98bc4c120e5b7a6528f6a25
Phillip Despi <phillipbrind@gmail.com>
2020-04-21 20:56:17 +1200

    Modified user profile UI to handle card layout.

--- BloodBank_PBD/Scripts/custom.js
+++ BloodBank_PBD/Scripts/custom.js
@@ -1,174 +1,169 @@
-﻿// User Login Profile
-$("#logged-user").hover(function () {
-    $("#user-menu").fadeIn();
-});
+﻿$(document).ready(function () {
 
-$("#user-menu").hover(function () {
-    $("#user-menu").show();
-}, function () {
-    $("#user-menu").fadeOut();
-});
+    // Log in
+    $("#user").focus(function () {
+        $("#user-line").css("background-color", "#F1562A");
+    });
 
-$("#edit-btn").click(function () {
-    $("input").removeAttr("disabled");
-    $("#update-btn").show();
-});
+    $("#user").blur(function () {
+        $("#user-line").css("background-color", "#000");
+    });
 
-// Log in
-$("#user").focus(function () {
-    $("#user-line").css("background-color", "#F1562A");
-});
+    $("#password").focus(function () {
+        $("#password-line").css("background-color", "#F1562A");
+    });
 
-$("#user").blur(function () {
-    $("#user-line").css("background-color", "#000");
-});
+    $("#password").blur(function () {
+        $("#password-line").css("background-color", "#000");
+    });
 
-$("#password").focus(function () {
-    $("#password-line").css("background-color", "#F1562A");
-});
+    $("#user").hover(function () {
+        $("#user-line").css("height", "2px")
+    }, function () {
+        $("#user-line").css("height", "1px")
+    });
 
-$("#password").blur(function () {
-    $("#password-line").css("background-color", "#000");
-});
+    $("#password").hover(function () {
+        $("#password-line").css("height", "2px")
+    }, function () {
+        $("#password-line").css("height", "0.5px")
+    });
 
-$("#user").hover(function () {
-    $("#user-line").css("height", "2px")
-}, function () {
-    $("#user-line").css("height", "1px")
-});
+    // Sign Up
+    $("#firstname").focus(function () {
+        $("#fname-line").css("background-color", "#F1562A");
+    });
 
-$("#password").hover(function () {
-    $("#password-line").css("height", "2px")
-}, function () {
-    $("#password-line").css("height", "0.5px")
-});
+    $("#firstname").blur(function () {
+        $("#fname-line").css("background-color", "#000");
+    });
 
-// Sign Up
-$("#firstname").focus(function () {
-    $("#fname-line").css("background-color", "#F1562A");
-});
+    $("#lastname").focus(function () {
+        $("#lname-line").css("background-color", "#F1562A");
+    });
 
-$("#firstname").blur(function () {
-    $("#fname-line").css("background-color", "#000");
-});
+    $("#lastname").blur(function () {
+        $("#lname-line").css("background-color", "#000");
+    });
 
-$("#lastname").focus(function () {
-    $("#lname-line").css("background-color", "#F1562A");
-});
+    $("#age").focus(function () {
+        $("#age-line").css("background-color", "#F1562A");
+    });
 
-$("#lastname").blur(function () {
-    $("#lname-line").css("background-color", "#000");
-});
+    $("#age").blur(function () {
+        $("#age-line").css("background-color", "#000");
+    });
 
-$("#age").focus(function () {
-    $("#age-line").css("background-color", "#F1562A");
-});
+    $("#address").focus(function () {
+        $("#address-line").css("background-color", "#F1562A");
+    });
 
-$("#age").blur(function () {
-    $("#age-line").css("background-color", "#000");
-});
+    $("#address").blur(function () {
+        $("#address-line").css("background-color", "#000");
+    });
 
-$("#address").focus(function () {
-    $("#address-line").css("background-color", "#F1562A");
-});
+    $("#blood-type").focus(function () {
+        $("#bloodtype-line").css("background-color", "#F1562A");
+    });
 
-$("#address").blur(function () {
-    $("#address-line").css("background-color", "#000");
-});
+    $("#blood-type").blur(function () {
+        $("#bloodtype-line").css("background-color", "#000");
+    });
 
-$("#blood-type").focus(function () {
-    $("#bloodtype-line").css("background-color", "#F1562A");
-});
+    $("#username").focus(function () {
+        $("#username-line").css("background-color", "#F1562A");
+    });
 
-$("#blood-type").blur(function () {
-    $("#bloodtype-line").css("background-color", "#000");
-});
+    $("#username").blur(function () {
+        $("#username-line").css("background-color", "#000");
+    });
 
-$("#username").focus(function () {
-    $("#username-line").css("background-color", "#F1562A");
-});
+    $("#confirm-pass").focus(function () {
+        $("#confirm-pass-line").css("background-color", "#F1562A");
+    });
 
-$("#username").blur(function () {
-    $("#username-line").css("background-color", "#000");
-});
+    $("#confirm-pass").blur(function () {
+        $("#confirm-pass-line").css("background-color", "#000");
 
-$("#confirm-pass").focus(function () {
-    $("#confirm-pass-line").css("background-color", "#F1562A");
-});
+        if ($("#confirm-pass").val() == "") {
+            $("#error-message").toggle();
+        }
+    });
 
-$("#confirm-pass").blur(function () {
-    $("#confirm-pass-line").css("background-color", "#000");
+    $("#firstname").hover(function () {
+        $("#fname-line").css("height", "2px")
+    }, function () {
+        $("#fname-line").css("height", "0.5px")
+    });
+
+    $("#lastname").hover(function () {
+        $("#lname-line").css("height", "2px")
+    }, function () {
+        $("#lname-line").css("height", "1px")
+    });
+
+    $("#age").hover(function () {
+        $("#age-line").css("height", "2px")
+    }, function () {
+        $("#age-line").css("height", "0.5px")
+    });
+
+    $("#address").hover(function () {
+        $("#address-line").css("height", "2px")
+    }, function () {
+        $("#address-line").css("height", "0.5px")
+    });
+
+    $("#lastname").hover(function () {
+        $("#lname-line").css("height", "2px")
+    }, function () {
+        $("#lname-line").css("height", "1px")
+    });
+
+    $("#blood-type").hover(function () {
+        $("#bloodtype-line").css("height", "2px")
+    }, function () {
+        $("#bloodtype-line").css("height", "1px")
+    });
+
+    $("#username").hover(function () {
+        $("#username-line").css("height", "2px")
+    }, function () {
+        $("#username-line").css("height", "0.5px")
+    });
+
+    $("#confirm-pass").hover(function () {
+        $("#confirm-pass-line").css("height", "2px")
+    }, function () {
+        $("#confirm-pass-line").css("height", "1px")
+    });
 
     if ($("#confirm-pass").val() == "") {
         $("#error-message").toggle();
     }
-});
 
-$("#firstname").hover(function () {
-    $("#fname-line").css("height", "2px")
-}, function () {
-    $("#fname-line").css("height", "0.5px")
-});
+    $("#signup-btn").click(function () {
+        if ($("#confirm-pass").val() != $("#password").val()) {
+            $("#error-message").toggle();
+            return false;
+        }
+    });
 
-$("#lastname").hover(function () {
-    $("#lname-line").css("height", "2px")
-}, function () {
-    $("#lname-line").css("height", "1px")
-});
+    // User Profile
+    $("#edit-btn").click(function () {
+        $("input").removeAttr("disabled");
+        $(".username-card").show();
+        $(".password-card").show();
+    });
 
-$("#age").hover(function () {
-    $("#age-line").css("height", "2px")
-}, function () {
-    $("#age-line").css("height", "0.5px")
-});
-
-$("#address").hover(function () {
-    $("#address-line").css("height", "2px")
-}, function () {
-    $("#address-line").css("height", "0.5px")
-});
-
-$("#lastname").hover(function () {
-    $("#lname-line").css("height", "2px")
-}, function () {
-    $("#lname-line").css("height", "1px")
-});
-
-$("#blood-type").hover(function () {
-    $("#bloodtype-line").css("height", "2px")
-}, function () {
-    $("#bloodtype-line").css("height", "1px")
-});
-
-$("#username").hover(function () {
-    $("#username-line").css("height", "2px")
-}, function () {
-    $("#username-line").css("height", "0.5px")
-});
-
-$("#confirm-pass").hover(function () {
-    $("#confirm-pass-line").css("height", "2px")
-}, function () {
-    $("#confirm-pass-line").css("height", "1px")
-});
-
-if ($("#confirm-pass").val() == "") {
-    $("#error-message").toggle();
-}
-
-$("#signup-btn").click(function () {
-    if ($("#confirm-pass").val() != $("#password").val()) {
-        $("#error-message").toggle();
-        return false;
-    }
-});
-
-// Admin
-$(document).ready(function () {
-    $("#search-lastname").on("keyup", function () {
-        var value = $(this).val().toLowerCase();
-        $("#user-table tr").filter(function () {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    // Admin
+    $(document).ready(function () {
+        $("#search-lastname").on("keyup", function () {
+            var value = $(this).val().toLowerCase();
+            $("#user-table tr").filter(function () {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
         });
     });
+
 });
